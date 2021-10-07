7d7bda9a707d95deabcc5f225f6878b34db1e6f9
Archana-bc <77508923+Archana-bc@users.noreply.github.com>
2021-10-06 23:56:12 -0400

    Update helmetdistribution.py

--- helmetdistribution.py
+++ helmetdistribution.py
@@ -1,22 +1,27 @@
 #using pandas to read the data sets into pandas data frame
-import pandas
+import pandas as pd
+pd.options.mode.chained_assignment = None
 
 #using matplot to diplay the graph
 import matplotlib.pyplot as plt
 
 # Reading the data set
-pdf = pandas.read_csv("/Users/rmkchowdary/Downloads/image_labels.csv")
+pdf = pd.read_csv("/Users/rmkchowdary/Downloads/image_labels.csv")
 
 #Data preparation & Cleaning
 newdf = pdf[(pdf.label == 'Helmet') | (pdf.label == 'Helmet-Blurred')]
 
+
+newdf['image'] = newdf['image'].str[ : 5]
 # finding the number helmets in each vedio based on image frames 
 finaldf = newdf.groupby("image").size()
 finaldf = finaldf.reset_index(name = "helmet_count")
 
 # Setting X and Y axis
 
-finaldf.plot(x = "image" , y = "helmet_count")
-
+finaldf.plot(x = "image" , y = "helmet_count" ,color = 'g')
+plt.xlabel("Video")
+plt.ylabel("helmet count")
 # Plotting the Graph
 plt.show()
+
